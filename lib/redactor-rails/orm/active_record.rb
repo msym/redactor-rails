3b018962f6deea70dbbe4b62dc940bc2e9ccc1f4
Maxim A Parfenov <maksym.0xff@gmail.com>
2013-04-23 11:35:49 +0600

    :dependent   => :destroy

--- lib/redactor-rails/orm/active_record.rb
+++ lib/redactor-rails/orm/active_record.rb
@@ -15,8 +15,7 @@ module RedactorRails
               self.table_name = "redactor_assets"
 
               belongs_to :assetable, :polymorphic => true
-              belongs_to :user,      :dependent   => :destroy
-
+              belongs_to :user
               attr_accessible :data, :assetable_type, :assetable_id, :assetable
             end
           end
